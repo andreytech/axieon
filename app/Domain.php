50bb7e3dc745d1c6c176776c60de96bbdbbfc829
Andrey Khomyakov <yourwebwizard@gmail.com>
2020-10-07 00:33:35 +0300

    models

--- app/Domain.php
+++ app/Domain.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property-read int id
@@ -12,4 +13,9 @@ class Domain extends Model
     protected $fillable = ['domain', 'brand_name', 'is_from_serp', 'is_major', 'is_minor', 'is_local'];
 
     public $timestamps = false;
+
+    public function pages(): HasMany
+    {
+        return $this->hasMany(Page::class);
+    }
 }
--- app/Page.php
+++ app/Page.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Page extends Model
 {
     protected $fillable = ['path', 'domain_id', 'subdomain', 'is_from_serp'];
 
     public $timestamps = false;
+
+    public function domain(): BelongsTo
+    {
+        return $this->belongsTo(Domain::class);
+    }
 }
